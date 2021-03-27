0dbcd7610367624f7414e06174bc163ff1680bdc
spdbear <16793732+spdbear@users.noreply.github.com>
2021-03-28 02:22:12 +0900

    botを作成

---
+++ bot/data.ts
@@ -0,0 +1,608 @@
+export const tweets = [
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1267455467594244097",
+    "image": "https://pbs.twimg.com/media/EZboB2jUwAI6pLl.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1272136235658522624",
+    "image": "https://pbs.twimg.com/media/EaeI8XhU8AALt9f.png"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1272363330259128321",
+    "image": "https://pbs.twimg.com/media/EahXs6FUcAAFm9g.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1267440496260993026",
+    "image": "https://pbs.twimg.com/media/EZbaaWiU4AEUYki.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1340945935610736641",
+    "image": "https://pbs.twimg.com/media/Epv_MNNUUAIa6Tn.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1283279443641810944",
+    "image": "https://pbs.twimg.com/media/Ec8f2BWU0AAmbsK.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1272959335505620993",
+    "image": "https://pbs.twimg.com/media/EHAPpkkUcAE6EHS.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1269584293581352960",
+    "image": "https://pbs.twimg.com/media/EZ54KjhUcAAxyD-.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1336594922149801986",
+    "image": "https://pbs.twimg.com/media/EoyJ-vmVEAIo2Kb.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1274642726143668224",
+    "image": "https://pbs.twimg.com/media/EbBwwciVAAAJMgP.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1277436199276122118",
+    "image": "https://pbs.twimg.com/media/EbpdbuYUwAAoRr_.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1286130458644721665",
+    "image": "https://pbs.twimg.com/media/EdlA1ZCUYAA8gxd.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1296402546215587842",
+    "image": "https://pbs.twimg.com/media/Ef2_PILUYAAkjih.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1273480068266815489",
+    "image": "https://pbs.twimg.com/media/EaxPUljVcAAm2Ov.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1279444386225418242",
+    "image": "https://pbs.twimg.com/media/EcF_Q6SUEAAiMqa.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1278757284412157953",
+    "image": "https://pbs.twimg.com/media/EYWipB5UEAEXF1u.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1310141567307456512",
+    "image": "https://pbs.twimg.com/media/Ei6Oyq5U4AApQfY.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1262706785707675648",
+    "image": "https://pbs.twimg.com/media/EHAPpkkUcAE6EHS.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1272166406184792065",
+    "image": "https://pbs.twimg.com/media/EaekgA3UMAAubQz.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1274959821205209088",
+    "image": "https://pbs.twimg.com/media/EbGRJjuU8AEBbgC.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1277074008442204160",
+    "image": "https://pbs.twimg.com/media/EbkUCmjUwAAxXCu.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1269162248175415297",
+    "image": "https://pbs.twimg.com/media/EZz4Vj7UMAAL2Y2.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1293472420251095040",
+    "image": "https://pbs.twimg.com/media/EfNWTZIU8AIhP-f.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1270203511746072576",
+    "image": "https://pbs.twimg.com/media/EaCrXFfU0AEsDUQ.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1267448069177503745",
+    "image": "https://pbs.twimg.com/media/EZbhSjoUwAEdJHb.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1269221806470119424",
+    "image": "https://pbs.twimg.com/media/EZ0uf2UU8AExBLd.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1290935077312970753",
+    "image": "https://pbs.twimg.com/media/EepSlL0UwAAjt-L.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1290603725292072960",
+    "image": "https://pbs.twimg.com/media/EeklPZnUMAEsI_u.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1272211766508838912",
+    "image": "https://pbs.twimg.com/media/EafN25mUwAEVkcq.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1304345104312799232",
+    "image": "https://pbs.twimg.com/media/Ehn2827VkAAnPBS.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1271079242697039874",
+    "image": "https://pbs.twimg.com/media/EaPHnXvUYAAWDaH.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1276832161312587776",
+    "image": "https://pbs.twimg.com/media/Ebg4FItUwAgky44.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1277783722666766336",
+    "image": "https://pbs.twimg.com/media/EbuZhcGUYAIKqrM.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1289561116863946759",
+    "image": "https://pbs.twimg.com/media/EeVw_sLU8AE90mv.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1269508895967305728",
+    "image": "https://pbs.twimg.com/media/EZ4zJpZU4AAEVLQ.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1262594100223565824",
+    "image": "https://pbs.twimg.com/media/EYWipB5UEAEXF1u.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1342669174586310662",
+    "image": "https://pbs.twimg.com/media/EqIeew-VgAE5ywe.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1272513739904839681",
+    "image": "https://pbs.twimg.com/media/EajgK2MUYAApUDS.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1273917940891557888",
+    "image": "https://pbs.twimg.com/media/Ea3dd_UUEAAcCRC.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1300719605816594433",
+    "image": "https://pbs.twimg.com/media/Eg0VkIEUMAArG6p.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1270928260965126145",
+    "image": "https://pbs.twimg.com/media/EaM-fWuU4AAaWf0.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1273827572367814656",
+    "image": "https://pbs.twimg.com/media/Ea2LbHvU0AAbrwS.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1263401366698881025",
+    "image": "https://pbs.twimg.com/media/EH4mSRDU0AEBhEn.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1298907820973101059",
+    "image": "https://pbs.twimg.com/media/EgalwQJU8AAEi8j.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1275382196639940608",
+    "image": "https://pbs.twimg.com/media/EbMRVzCUwAAEwWb.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1267342357332033537",
+    "image": "https://pbs.twimg.com/media/EZaBJ0RU0AAmIV-.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "buzz",
+    "answer": "https://twitter.com/Rhkya/status/1274582320842895360",
+    "image": "https://pbs.twimg.com/media/EbA51G2UMAIa4lb.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1272604354084605952",
+    "image": "https://pbs.twimg.com/media/EFoVIhZU4AAQUnN.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1270248784111394817",
+    "image": "https://pbs.twimg.com/media/EaDUiRRUwAMfFDa.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1307968663824154631",
+    "image": "https://pbs.twimg.com/media/EibWkMBU8AAmaug.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1279626193646436352",
+    "image": "https://pbs.twimg.com/media/EcIlPWSVcAMd6P8.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1336685391399669761",
+    "image": "https://pbs.twimg.com/media/EozcQutVoAINKqk.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1268739533392076801",
+    "image": "https://pbs.twimg.com/media/EZt34JcU8AAjAVp.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1277855837029974016",
+    "image": "https://pbs.twimg.com/media/EbvbG6-U4AIKu2J.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1282207687577485312",
+    "image": "https://pbs.twimg.com/media/EctRGF2UcAALE6N.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1341669783721283584",
+    "image": "https://pbs.twimg.com/media/Ep6RiqsU0AAvq1Q.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1277179461163614208",
+    "image": "https://pbs.twimg.com/media/Eblz8uJU0AAQ0Oi.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1278553514248032257",
+    "image": "https://pbs.twimg.com/media/Eb5VnpaU8AAQCl9.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1270875577268858881",
+    "image": "https://pbs.twimg.com/media/EFUiabwVUAA3KI2.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1273812434872983553",
+    "image": "https://pbs.twimg.com/media/Ea19qDJUYAA1VaA.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1277979709419077635",
+    "image": "https://pbs.twimg.com/media/EbxLskXU0AA07ZO.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1276016593869271040",
+    "image": "https://pbs.twimg.com/media/EbVSUy-VcAErUGO.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1272340052870180866",
+    "image": "https://pbs.twimg.com/media/EahCb81U4AAB6pV.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1337411779425550340",
+    "image": "https://pbs.twimg.com/media/Eo9w6fDVQAEtz8g.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1269538921009958912",
+    "image": "https://pbs.twimg.com/media/EZ5O6thU0AAmsl6.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1279640520084447234",
+    "image": "https://pbs.twimg.com/media/EcIyQW7U8AAUxLZ.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1274718374266732544",
+    "image": "https://pbs.twimg.com/media/EbC1moUU4AIZn7Q.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1288020996071415809",
+    "image": "https://pbs.twimg.com/media/Ed_4QffUcAAZyu9.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1285892199431802880",
+    "image": "https://pbs.twimg.com/media/EdhoI4dVcAEOz5g.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1277874015147704320",
+    "image": "https://pbs.twimg.com/media/EbvrKzYVAAANKAZ.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1269266348338626560",
+    "image": "https://pbs.twimg.com/media/EZ1XA-dU8AAIV93.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1272234518871543808",
+    "image": "https://pbs.twimg.com/media/EFeS8zdU8AA0-qo.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1276515215950049283",
+    "image": "https://pbs.twimg.com/media/EbcX0mDUcAY6qcY.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1297299451007000576",
+    "image": "https://pbs.twimg.com/media/Ea1jHZdXYAAaYtx.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1275295637265723393",
+    "image": "https://pbs.twimg.com/media/EbLCnvQU0AE80to.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1281136032352632832",
+    "image": "https://pbs.twimg.com/media/EceAbZVUMAAmBFT.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1293879477269798913",
+    "image": "https://pbs.twimg.com/media/EfTIdIKUMAE92Nr.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1277496531633897474",
+    "image": "https://pbs.twimg.com/media/EbqUUrFU4AEapwG.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1288126591483047938",
+    "image": "https://pbs.twimg.com/media/EeBYQFYUwAom3yj.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1274355833602584577",
+    "image": "https://pbs.twimg.com/media/Ea9rz0pUEAYN1E5.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1274023634877595648",
+    "image": "https://pbs.twimg.com/media/Ea49q_wUEAA2Qxu.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1334058901784780800",
+    "image": "https://pbs.twimg.com/media/EoOHf7cVQAIpRjN.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1327807364494331904",
+    "image": "https://pbs.twimg.com/media/Em1Rv6mVkAAcRcE.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1267985239445389312",
+    "image": "https://pbs.twimg.com/media/EZjJ2lPU4AE1ttt.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1274831433052258304",
+    "image": "https://pbs.twimg.com/media/EJGk73CVAAEta_u.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1310050055219363840",
+    "image": "https://pbs.twimg.com/media/Ei47kirU4AAVzHA.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1274509859006738433",
+    "image": "https://pbs.twimg.com/media/Ea_39acVAAUUJIl.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1293381198484389888",
+    "image": "https://pbs.twimg.com/media/EfMDU-5UYAMORI1.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1273948221954351104",
+    "image": "https://pbs.twimg.com/media/Ea341BMUwAE7PH-.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1335963692299476992",
+    "image": "https://pbs.twimg.com/media/EopL40CUcAAbuAs.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1273396996393496577",
+    "image": "https://pbs.twimg.com/media/EHfHbufUEAAfEIw.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1278160961841422336",
+    "image": "https://pbs.twimg.com/media/EbzwnKIUEAAnS9y.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1277239825565749250",
+    "image": "https://pbs.twimg.com/media/EbmqvmvUcAEEgtv.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1274597606438481921",
+    "image": "https://pbs.twimg.com/media/EbBGv1EU8AAjHWk.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1270688608685445120",
+    "image": "https://pbs.twimg.com/media/EaJkjWFUYAEJInZ.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1278613460889202689",
+    "image": "https://pbs.twimg.com/media/Eb6MHFuUYAA9Ap3.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1289863004394098689",
+    "image": "https://pbs.twimg.com/media/EeaDiLyVAAAdSCR.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1277811177364443136",
+    "image": "https://pbs.twimg.com/media/Ebuyff8U0AA-4X-.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1276605624613105664",
+    "image": "https://pbs.twimg.com/media/EW7ZxCPUEAEGBgl.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1266594824376905730",
+    "image": "https://pbs.twimg.com/media/EZPZRXlUcAAc69A.jpg"
+  },
+  {
+    "account": "Rhkya",
+    "type": "nobuzz",
+    "answer": "https://twitter.com/Rhkya/status/1270822731605393409",
+    "image": "https://pbs.twimg.com/media/EFUihN6UEAAfUhJ.jpg"
+  }
+]
---
+++ bot/index.ts
@@ -0,0 +1,44 @@
+import { Client } from 'discord.js'
+import { config } from './config'
+import { tweets } from './data';
+
+const client = new Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user}!`)
+})
+
+type State = 'NOT_QUESTION' | 'QUESTIONING'
+let botState: State = 'NOT_QUESTION'
+type Tweet = {
+    account: string;
+    type: string;
+    answer: string;
+    image: string;
+}
+
+let tweetTuple: [Tweet, Tweet]
+const buzzedList = tweets.filter((v) => v.type === 'buzz')
+const nobuzzList = tweets.filter((v) => v.type === 'nobuzz')
+
+console.log(buzzedList,nobuzzList)
+
+client.on('message', (msg) => {
+  if (msg.content === '!quiz' && botState === 'NOT_QUESTION') {
+    const buzzedTweet = buzzedList[Math.floor(buzzedList.length * Math.random())]
+    const nobuzzTweet = nobuzzList[Math.floor(nobuzzList.length * Math.random())]
+    const seed = Math.floor((Math.random() * 100)) % 2
+    tweetTuple = seed < 1 ? [buzzedTweet, nobuzzTweet] : [nobuzzTweet, buzzedTweet]
+    console.log(seed)
+    msg.reply(
+      `どっちがバズった？\n${tweetTuple[0].image}\n${tweetTuple[1].image}`
+    )
+    botState = 'QUESTIONING'
+  }
+  if (msg.content === '!answer' && botState === 'QUESTIONING') {
+    msg.reply(`結果発表！\n${tweetTuple[0].answer}\n${tweetTuple[1].answer}`)
+    botState = 'NOT_QUESTION'
+  }
+})
+
+client.login(config.token)
